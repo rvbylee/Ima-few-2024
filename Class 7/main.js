ba384577e8d474d873fb37ff110b8a8a20b45abb
rvbylee <yl11452@nyu.edu>
2024-10-17 20:04:49 -0400

    Numbers Squared Exercise
    Numbers squared

---
+++ Class 7/main.js
@@ -0,0 +1,38 @@
+
+
+function squares(val){
+
+    for(let i = 1; i <= val; i++){
+
+        /* displayText.innerHTML += '<p> The Square of ' + i + ' is: ' +
+        (i * i) + '</p>';  */  
+//innerHTML is an element of what is what is in the body
+// displayText is an element
+
+        displayText.innerHTML += `
+        <p>
+            The square of ${i} is: ${i * i}.
+        </p>
+        `;
+    }               
+};
+
+goButton.onclick = function(){
+    squares(maxNumInput.value);
+};
+
+maxNumInput.oninput = function (){
+    squares(maxNumInput.value);
+};
+
+clearButton.onclick = function (){
+    displayText.innerHTML = '';
+    maxNumInput.value = '';
+};
+
+bgColorInput.onchange = function (){ //onchange is different from onclick, is changing something
+    document.body.style.backgroundColor = bgColorInput.value; //document... makes  it easier to access than giving an id to the body
+};
+
+
+
